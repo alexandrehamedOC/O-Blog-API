1348c55bf85c49b7c4d67b2eebea864d076adcd1
YannOclock <yann@oclock.io>
2024-05-31 09:36:07 +0200

    n,etooyage oubli

--- migrations/revert/post-views.sql
+++
@@ -1,7 +0,0 @@
--- Revert oblog:post-views from pg
-
-BEGIN;
-
-DROP VIEW "posts_with_category_label";
-
-COMMIT;
--- migrations/verify/post-views.sql
+++
@@ -1,7 +0,0 @@
--- Verify oblog:post-views on pg
-
-BEGIN;
-
-SELECT "id", "title", "slug", "excerpt", "content", "category" FROM "posts_with_category_label" WHERE false;
-
-ROLLBACK;
