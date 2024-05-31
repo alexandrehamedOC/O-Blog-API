c49c1fea46553e8f1d7a2e6e3dead47f3b6ab3a9
YannOclock <yann@oclock.io>
2024-05-31 09:35:00 +0200

    nettoyage

--- migrations/deploy/base-structure.sql
+++ migrations/deploy/base-structure.sql
@@ -4,19 +4,19 @@ BEGIN;
 
 CREATE TABLE "categories" (
   "id" int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  "label" TEXT NOT NULL UNIQUE,
-  "route" TEXT NOT NULL UNIQUE,
+  "label" text NOT NULL UNIQUE,
+  "route" text NOT NULL UNIQUE,
   "created_at" timestamptz NOT NULL DEFAULT now(),
   "updated_at" timestamptz
 );
 
 CREATE TABLE "posts" (
   "id" int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  "title" TEXT NOT NULL UNIQUE,
-  "slug" TEXT NOT NULL UNIQUE,
-  "excerpt" TEXT,
-  "content" TEXT,
-  "categories_id" INT NOT NULL REFERENCES "categories" ("id"),
+  "title" text NOT NULL UNIQUE,
+  "slug" text NOT NULL UNIQUE,
+  "excerpt" text,
+  "content" text,
+  "categories_id" int NOT NULL REFERENCES "categories" ("id"),
   "created_at" timestamptz NOT NULL DEFAULT now(),
   "updated_at" timestamptz
 );
