fe2283b111a518622676596961ad78f654d11f4b
YannOclock <yann@oclock.io>
2024-05-31 15:19:45 +0200

    test sans ssl

--- app/config/pg.client.js
+++ app/config/pg.client.js
@@ -5,7 +5,7 @@ const { Pool } = pg;
 
 // const client = new Client();
 // Grâce à la connexion de type Pool on permet à la BDD d'ouvrir automatiquement une nouvelle connexion pour un nouvel utilisateur/requête HTTP. Ce qui permet de ne pas avoir à attendre que la requête SQL précédente soit terminée pour en lancer une nouvelle.
-const client = new Pool({connectionString: process.env.PGURL, ssl:true});
+const client = new Pool({connectionString: process.env.PGURL});
 
 // client.connect();
 
